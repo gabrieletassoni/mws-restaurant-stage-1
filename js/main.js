386cc5440776339c8e2933c0fae85e3d50c89a6d
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-03-20 01:11:35 +0100

    fix: add alt to img tags

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} Picture`
   li.append(image);
 
   const name = document.createElement('h1');
